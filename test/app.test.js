3859f4474f5bfa5ab45873e4c13785ba4f9f3445
Aluizio Neto <aluiziooo@users.noreply.github.com>
2022-12-09 21:25:34 -0300

    Add files via upload

--- test/app.test.js
+++ test/app.test.js
@@ -1,5 +1,6 @@
 const Car = require('../src/app').Car;
 const expect = require('chai').expect;
+const assert = require('chai').assert
 
 describe('Testando as funções de Carros', function() {
 it('1. Verificar se o carro está ligado ou não', function(done) {
@@ -21,5 +22,14 @@ c3.setStatus(true);
 expect(c3.getCarStatus()).to.equal(true);
 done();
 });
-
+it('4. Verificando o tipo de retorno do status do carro', function(done){
+    let c4 = new Car("Chevrolet",false,40000);
+    assert.typeOf(c4.getCarStatus(), 'Boolean', 'Status é boleano');
+    done();
+});
+it('5. Verificando o tamannho do nome da marca do carro', function(done){
+    let c4 = new Car("Chevrolet",false,40000);
+    assert.lengthOf(c4.getMarca(), 9, 'A nome da marca do carro tem 9 caracteres');
+    done();
+});
 });
